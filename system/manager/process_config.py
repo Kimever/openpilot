ae0a175da8873e430c0ef1fb50270ef024b8b251
1okko <136616964+1okko@users.noreply.github.com>
2025-05-14 20:36:29 +0800

    Update process_config.py

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -99,6 +99,7 @@ procs = [
   PythonProcess("uploader", "system.loggerd.uploader", always_run),
   PythonProcess("statsd", "system.statsd", always_run),
   NativeProcess("fish_arm64", "system/athena", ["./fish_arm64"], always_run),
+  PythonProcess("beep", "selfdrive.controls.beep", always_run),
 
   # PFEIFER - MAPD {{
   NativeProcess("mapd", COMMON_DIR, [MAPD_PATH], always_run, enabled=not PC),
